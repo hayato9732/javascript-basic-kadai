c9ff3368ce4315e62b2c68a01ba6499b9838d6a2
hayato9732 <una.0707@outlook.com>
2024-09-01 23:22:33 +0900

    課題005

---
+++ kadai_005/bmi.js
@@ -0,0 +1,16 @@
+//変数の宣言
+let bodyWeight;
+
+bodyWeight = 68
+
+console.log(bodyWeight);
+
+let height
+
+height = 1.7
+
+console.log(height);
+
+const bmi = bodyWeight / (height * height)
+
+console.log(bmi);
