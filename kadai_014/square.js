1a7d35063007587dde3b3af4d8fab5feeb52efea
hayato9732 <una.0707@outlook.com>
2024-09-23 20:21:16 +0900

    課題　014

---
+++ kadai_014/square.js
@@ -0,0 +1,9 @@
+// 値を二乗する関数
+function square(num) {
+  const result = num * num; // 二乗の計算
+  console.log(result); // 計算結果をコンソールに出力
+  return result; // 計算結果を戻り値として返す
+}
+
+// 例: 関数を呼び出してみる
+square(10); // 100がコンソールに表示される
